d936191fc9498d97bd70a328cf8c5e3f4de500db
GrumWald <56920688+GrumWald@users.noreply.github.com>
2020-07-22 11:32:48 +0200

    Simplifying TipForm.js using functional component

--- src/components/TipForm.js
+++ src/components/TipForm.js
@@ -1,73 +1,59 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { useState } from 'react';
 
-class TipForm extends Component {
-    constructor ( props ) {
-        super( props );    
-        this.state = {
-            inputAmount: 1,
-            quality: '1.1',
-            guests: 1,
-            finalAmount: 0
-        }
-        this.handleInputChange = this.handleInputChange.bind( this );
-        this.calcFinalAmount = this.calcFinalAmount.bind( this );
-    }
+function TipForm() {
+    const [ inputAmount, setInputAmount ] = useState( 1 );
+    const [ quality, setQuality ] = useState( '1.1' );
+    const [ guests, setGuests ] = useState( 1 );
+    const [ finalAmount, setFinalAmount ] = useState( 0 );
 
-    handleInputChange( e ) {
-        this.setState( {
-            [ e.target.name ]: +e.target.value,
-            finalAmount: 0
-        } );
-    }
-
-    calcFinalAmount( e ) {
+    const calcFinalAmount = ( e ) => {
         const fixTip = 10;
         e.preventDefault();
         let finalAmount = 0;
-        finalAmount = this.state.inputAmount * +this.state.quality;
-        if ( this.state.guests > 1 ) {
-            finalAmount += ( this.state.guests - 1 ) * fixTip;
+        finalAmount = inputAmount * +quality;
+        if ( guests > 1 ) {
+            finalAmount += ( guests - 1 ) * fixTip;
         }
         finalAmount = Math.ceil( finalAmount / 10 ) * 10;
-        this.setState( { finalAmount: +finalAmount } );
-    }
+        setFinalAmount( finalAmount );
+    };
 
-    render() {
-        return (
-            <form className='tip-form' onSubmit={this.calcFinalAmount}>
-                <label>ČÁSTKA NA ÚČTU</label>
-                <input
-                    type='number'
-                    value={this.state.inputAmount}
-                    name='inputAmount'
-                    min='1'
-                    className='form-input'
-                    onChange={this.handleInputChange}
-                />
+    return (
+        <form className='tip-form' onSubmit={calcFinalAmount}>
+            <label>ČÁSTKA NA ÚČTU</label>
+            <input
+                type='number'
+                value={inputAmount}
+                name='inputAmount'
+                min='1'
+                className='form-input'
+                onChange={e => setInputAmount(e.target.value)}
+            />
 
-                <label>KVALITA OBSLUHY</label>
-                <select value={this.state.quality} name='quality' onChange={this.handleInputChange}>
-                    <option value='1.02'>ŠPATNÁ</option>
-                    <option value='1.1'>DOBRÁ</option>
-                    <option value='1.15'>PERFEKTNÍ, JSEM NADŠENÝ</option>
-                </select>
+            <label>KVALITA OBSLUHY</label>
+            <select value={quality} name='quality' onChange={e => setQuality( e.target.value)}>
+                <option value='1.02'>ŠPATNÁ</option>
+                <option value='1.1'>DOBRÁ</option>
+                <option value='1.15'>PERFEKTNÍ, JSEM NADŠENÝ</option>
+            </select>
 
-                <label>POČET HOSTŮ</label>
-                <input
-                    type='number'
-                    value={this.state.guests}
-                    name='guests'
-                    min='1'
-                    className='form-input'
-                    onChange={this.handleInputChange}
-                />
-                <input type='submit' value='SPOČÍTAT ÚČET' />
+            <label>POČET HOSTŮ</label>
+            <input
+                type='number'
+                value={guests}
+                name='guests'
+                min='1'
+                className='form-input'
+                onChange={e => setGuests( e.target.value)}
+            />
+            <input type='submit' value='SPOČÍTAT ÚČET' />
 
-                <label>VÝSLEDNÝ ÚČET</label>
-                <p className='result'>{this.state.finalAmount},- Kč</p>
-            </form>
-        )
-    }
+            <label>VÝSLEDNÝ ÚČET</label>
+            <p className='result'>{finalAmount},- Kč</p>
+        </form>
+    );
+
 }
 
-export default TipForm
+export default TipForm;
